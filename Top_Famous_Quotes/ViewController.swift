23ffd35d0f5d48466e84ead280ec0b53cced3926
Muhammad Basalamah <muhammad@Testtests-MacBook-Pro.local>
2018-10-12 14:23:19 +0700

    Initial Commit

---
+++ Top_Famous_Quotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Top_Famous_Quotes
+//
+//  Created by Muhammad Basalamah on 12/10/18.
+//  Copyright © 2018 Mubs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
